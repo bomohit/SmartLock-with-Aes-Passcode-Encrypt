72cc1c59ed954817e3fbd408ab72757c460c7cd1
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-03 14:44:14 +0800

    create layout and class for SignUP

---
+++ app/src/main/java/com/example/smartlock/SelectionSignUp.kt
@@ -0,0 +1,12 @@
+package com.example.smartlock
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectionSignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.selection_signup)
+
+    }
+}
