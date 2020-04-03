0953e3a7389c9207b925561f76f384343892354b
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-03 15:42:23 +0800

    add logic for selectionMain

--- app/src/main/java/com/example/smartlock/SelectionMain.kt
+++ app/src/main/java/com/example/smartlock/SelectionMain.kt
@@ -1,13 +1,25 @@
 package com.example.smartlock
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smartlock.R
+import kotlinx.android.synthetic.main.selection_main.*
 
 class SelectionMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.selection_main)
 
+        buttonLogin.setOnClickListener {
+            startActivity(Intent(this, SelectionLogin::class.java))
+        }
+
+        buttonSignUp.setOnClickListener {
+            startActivity(Intent(this, SelectionSignUp::class.java))
+        }
     }
+
+
 }
