27616972d33d0f2db288db7fdd8556fa70e01e42
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-03 14:31:52 +0800

    creat layout and class for selection main

---
+++ app/src/main/java/com/example/smartlock/ui/SelectionMain.kt
@@ -0,0 +1,13 @@
+package com.example.smartlock.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.smartlock.R
+
+class SelectionMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.selection_main)
+
+    }
+}
