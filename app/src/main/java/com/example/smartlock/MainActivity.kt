bed765111fd25d35152c52eec1dfe58290efdf1c
Kaitou Ryz <faryz.coder@gmail.com>
2020-04-06 00:44:08 +0800

    add sign out option menu and alert dialog

--- app/src/main/java/com/example/smartlock/MainActivity.kt
+++ app/src/main/java/com/example/smartlock/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.smartlock
 
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -32,7 +40,58 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+            R.id.action_signOut -> {
+                signOut()
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
 
 
     }
+
+    private fun signOut(): Boolean {
+        d("bomohit", "sign out pressed")
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Signing Out?")
+        builder.setMessage("Confirm?")
+
+        val yes = "yes".toString()
+
+        builder.setPositiveButton("YES", DialogInterface.OnClickListener {
+            dialog, id ->
+            Toast.makeText(applicationContext,"Bye", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, SelectionMain::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                    Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish() // finish the current activity
+        })
+
+        builder.setNegativeButton("No"){dialog, id ->
+            Toast.makeText(applicationContext,"Cancelled", Toast.LENGTH_SHORT).show()
+        }
+
+        builder.setPositiveButtonIcon(resources.getDrawable(R.drawable.ic_check_black_24dp))
+        builder.setNegativeButtonIcon(resources.getDrawable(R.drawable.ic_clear_black_24dp))
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+
 }
